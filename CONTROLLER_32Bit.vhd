ac6c3f23c236012c7d8ea4320c9498c5f9d6ec36
David Pynes <Dpynes@gmail.com>
2016-02-14 20:12:41 -0800

    update added addiu

--- CONTROLLER_32Bit.vhd
+++ CONTROLLER_32Bit.vhd
@@ -385,6 +385,24 @@ BEGIN
 				t_ALUControl        <= "100000";
 				t_ALUSrc            <= '1';	
 				t_RegWrite          <= '1';	
+				
+			ELSIF (Op = "001000") THEN --addiu
+				t_JALControl        <= '0';
+				t_RegDst            <= '0';
+				t_JALAddrControl    <= '0';
+				t_JALDataControl    <= '1';
+				t_ShiftValueControl <= '0';
+				t_LoadControl       <= '0';
+				t_JRControl         <= '0';
+				t_JumpOrJRControl   <= '1';
+				t_LUIControl        <= '1';
+				t_Branch            <= '0';
+				t_MemToReg          <= '0';
+				t_MemWrite          <= '1';
+				t_DSize             <= "11";
+				t_ALUControl        <= "100001";
+				t_ALUSrc            <= '1';	
+				t_RegWrite          <= '1';	
 		------- -- -- -- -- -- -- -- -- --  ----- 			
 			ELSIF (Op = "000100") THEN --BRANCH ON EQUAL-- IS ITYPE 
 				t_JALControl        <= '0';
